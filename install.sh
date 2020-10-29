fbed003e9438a31ee639465980b63d2a5fdc5757
HassanZahin <zahinnowshin123@gmail.com>
2020-10-29 08:14:19 +0600

    Refractor code

--- install.sh
+++ install.sh
@@ -7,13 +7,13 @@ sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
 sudo apt-add-repository https://cli.github.com/packages
 sudo apt update
 sudo apt install gh git wget firefox
-pip3 install -r ./requirements.txt
+pip3 install -r bin/requirements.txt
 
 echo Copying files
 mkdir ~/.creatify
-cp ./bin/{configure.sh,Creatify-GithubCLI.sh,Creatify-WebScraping.sh,creatify,webtemplate.sh,create.py} ~/.creatify/
+cp bin/* ~/.creatify/
 
 echo export PATH=$PATH:~/.creatify >> ~/.bashrc
-chmod u+x "~/.creatify/creatify"
+chmod +x ~/.creatify/creatify
 
 echo "Please restart the terminal to start using Creatify.."
