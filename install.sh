031bea326ea8e7cd5ff9e9fdb1fac63ef2f8abd6
HassanZahin <zahinnowshin123@gmail.com>
2020-10-29 07:42:07 +0600

    Refactor code

--- install.sh
+++ install.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-echo "We will install some required linux and python programs and packages first."
+echo "Installing packages"
 
+sudo apt install apt-transport-http software-properties-commons
 sudo apt-add-repository https://cli.github.com/packages
-sudo apt install apt-transport-https
 sudo apt update
-sudo apt install gh git wget
+sudo apt install gh git wget firefox
 pip3 install -r ./requirements.txt
 
-cp ./bin/{configure.sh,Creatify-GithubCLI.sh,Creatify-WebScraping.sh,creatify,webtemplate.sh,create.py} "/home/mohammadarik/.creatify/"
-# cp -r
+cp ./bin/{configure.sh,Creatify-GithubCLI.sh,Creatify-WebScraping.sh,creatify,webtemplate.sh,create.py} ~/.creatify/
 
-echo "export PATH=\"\$PATH:/home/mohammadarik/.creatify\"" >> "/home/mohammadarik/.bashrc"
-chmod u+x "/home/mohammadarik/.creatify/creatify"\
+echo export PATH=$PATH:~/.creatify >> ~/.bashrc
+chmod u+x "~/.creatify/creatify"
 
 echo "Please restart the terminal to start using Creatify.."
