f911c8fbe7a354bc4c63d2aabf96f3ab208ece19
HassanZahin <zahinnowshin123@gmail.com>
2020-10-29 07:57:32 +0600

    Refactor code

--- install.sh
+++ install.sh
@@ -2,12 +2,14 @@
 
 echo "Installing packages"
 
-sudo apt install apt-transport-http software-properties-commons
+sudo apt install apt-transport-https software-properties-common
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
 sudo apt-add-repository https://cli.github.com/packages
 sudo apt update
 sudo apt install gh git wget firefox
 pip3 install -r ./requirements.txt
 
+echo Copying files
 cp ./bin/{configure.sh,Creatify-GithubCLI.sh,Creatify-WebScraping.sh,creatify,webtemplate.sh,create.py} ~/.creatify/
 
 echo export PATH=$PATH:~/.creatify >> ~/.bashrc
