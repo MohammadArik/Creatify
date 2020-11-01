45b607ea2f76847d4a79f6a1f02067b6503afcb3
MohammadArik <mohammadarik63@gmail.com>
2020-11-01 20:41:38 +0600

    Development Files Removed

--- bin/creds.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-python="/home/mohammadarik/Arik/Python Projects/" 
-flutter="/home/mohammadarik/Arik/Flutter Projects/" 
-web="/home/mohammadarik/Arik/Web Projects/" 
-node="/home/mohammadarik/Arik/Node Projects/" 
-default="/home/mohammadarik/Arik/Projects/"
-editor="code"
